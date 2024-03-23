5f9b73acdc81582c6a86adbaa71a43670b1fd41a
Darcy Paterson <darcypaterson@gmail.com>
2024-03-23 10:38:10 -0600

    small change

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Dude
+          Learn React Dude To Make It Happen
         </a>
       </header>
     </div>
